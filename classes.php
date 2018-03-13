9516952c43a4e7201f2a36fc822cae91854fe9ed
Kai Summers <kaisummers@users.noreply.github.com>
2018-03-14 03:32:51 +0800

    Update classes.php

--- classes.php
+++ classes.php
@@ -7,14 +7,16 @@ class intEncode
     const CHARS = "doW4J8l02v"; // Replace with your own 10 random characters
     public static function encode($n)
     {
-        foreach(str_split($n) as $v){
+        foreach(str_split($n) as $v)
+        {
             $o[] = self::CHARS[$v];
         }
         return implode($o);
     }
     public static function decode($n)
     {
-        foreach(str_split($n) as $v){
+        foreach(str_split($n) as $v)
+        {
             $o[] = strpos(self::CHARS,$v);
         }
         return implode($o);
